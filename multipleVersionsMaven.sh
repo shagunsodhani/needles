3ba2da4946414f07ffa356841cc6298041563f0a
Shagun <sshagunsodhani@gmail.com>
2015-10-30 09:31:09 +0530

    added the path to binary

--- multipleVersionsMaven.sh
+++ multipleVersionsMaven.sh
@@ -1,7 +1,7 @@
 wget http://www.eu.apache.org/dist/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz
 tar -zxf apache-maven-3.3.3-bin.tar.gz
 sudo mv apache-maven-3.3.3 /usr/share/maven3.3.3
-sudo update-alternatives --install /usr/bin/mvn mvn /usr/share/maven3.3.3/ 200
+sudo update-alternatives --install /usr/bin/mvn mvn /usr/share/maven3.3.3/bin/mvn 200
 sudo update-alternatives --config mvn
 echo "alias choosemvn='sudo update-alternatives --config mvn'">>~/.bashrc
 . ~/.bashrc
