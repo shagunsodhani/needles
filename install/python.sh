a09aae4ab4d6e5065ceb781b0ad5e9f14fe7278c
shagun Sodhani <sshagunsodhani@gmail.com>
2016-08-18 13:06:29 +0530

    Moved installation scripts to seperate dir and added python package list

---
+++ install/python.sh
@@ -0,0 +1,8 @@
+sudo apt-get update 
+sudo apt-get -y install python-pip
+
+sudo apt-get install -y pkg-config libblas-dev liblapack-dev gfortran python-dev g++ libfreetype6-dev libpng-dev libjpeg8-dev
+
+#python packages
+sudo apt-get install python-matplotlib python-scipy scikit-learn
+sudo pip install numpy sympy pandas jupyter seaborn csvkit
