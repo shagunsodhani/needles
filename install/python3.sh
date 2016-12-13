c12d4f541361a079fd01247eb325586527c17cbb
shagun Sodhani <sshagunsodhani@gmail.com>
2016-12-13 21:09:43 +0530

    Added seperate scripts for python2 and python3

---
+++ install/python3.sh
@@ -0,0 +1,7 @@
+sudo apt-get update 
+sudo apt-get install python3-pip
+
+sudo apt-get install -y pkg-config libblas-dev liblapack-dev gfortran python-dev g++ libfreetype6-dev libpng-dev libjpeg8-dev
+
+#python packages
+sudo pip3 install numpy sympy pandas jupyter seaborn csvkit matplotlib scipy scikit-learn
